1ec07bbec01e35997c05b7476d9e20e648ef8f9b
Frank Pfattheicher <fpf@ict-baden.de>
2019-03-07 16:06:21 +0100

    Removed not more used code.

--- BasicCrossPlatform/Program.cs
+++ BasicCrossPlatform/Program.cs
@@ -39,24 +39,9 @@ namespace BasicCrossPlatform
             var appDirectory = AppDomain.CurrentDomain.BaseDirectory;
             Console.WriteLine($"App directory is {appDirectory}");
             var startUrl = $"file:///{appDirectory}/index.html";
-            //var startUrl = $"https://google.de";
 
             var config = ChromelyConfiguration
                 .Create()
-
-                //TODO: check linux defaults
-                // Set multi-threaded_message_loop false
-                // only supported on windows
-                //.WithCustomSetting(CefSettingKeys.MultiThreadedMessageLoop, false)
-                //.WithCustomSetting(CefSettingKeys.SingleProcess, true)
-                //.WithCustomSetting(CefSettingKeys.NoSandbox, true)
-
-                //.WithCommandLineArg("disable-extensions", "1")
-                //.WithCommandLineArg("disable-gpu", "1")
-                //.WithCommandLineArg("disable-gpu-compositing", "1")
-                //.WithCommandLineArg("disable-smooth-scrolling", "1")
-                //.WithCommandLineArg("no-sandbox", "1")
-
                 .WithAppArgs(args)
                 .WithHostSize(1000, 600)
                 .WithStartUrl(startUrl);
