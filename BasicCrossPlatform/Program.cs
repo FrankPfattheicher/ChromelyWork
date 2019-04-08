954ba90a1b34b7acbaf8cc9e96598c2cb7f85728
Frank Pfattheicher <fpf@ict-baden.de>
2019-04-08 22:00:17 +0200

    Code cleanup, frameless test.

--- BasicCrossPlatform/Program.cs
+++ BasicCrossPlatform/Program.cs
@@ -13,29 +13,6 @@ namespace BasicCrossPlatform
     {
         private static int Main(string[] args)
         {
-            //try
-            //{
-            //    var platform = CefRuntime.Platform;
-            //    var version = CefRuntime.ChromeVersion;
-            //    Console.WriteLine($"Running {platform} chromium {version}");
-
-            //    try
-            //    {
-            //        CefRuntime.Load();
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        Debug.WriteLine(ex.Message);
-            //        CefLoader.Load();
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine(ex);
-            //    Environment.Exit(0);
-            //}
-
-
             var appDirectory = AppDomain.CurrentDomain.BaseDirectory;
             Console.WriteLine($"App directory is {appDirectory}");
             var startUrl = $"file:///{appDirectory}/index.html";
@@ -43,6 +20,7 @@ namespace BasicCrossPlatform
             var config = ChromelyConfiguration
                 .Create()
                 .WithLoadingCefBinariesIfNotFound(true)
+                .WithFramelessHost()
                 .WithAppArgs(args)
                 .WithHostSize(1000, 600)
                 .WithStartUrl(startUrl);
@@ -50,7 +28,6 @@ namespace BasicCrossPlatform
             try
             {
                 using (var window = ChromelyBrowserWindow.Create(config))
-                //using (var window = new CefGlueBrowserWindow(config))
                 {
                     var result = window.Run(args);
                     Console.WriteLine("Run returns " + result);
